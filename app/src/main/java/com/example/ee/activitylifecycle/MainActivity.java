9df12cdc60dcf595d58b759053fb8daa798640d6
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-07-27 23:29:47 +0500

    added logCat with every Activity State

--- app/src/main/java/com/example/ee/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/ee/activitylifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ee.activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d("LifeCycle", "onCreate ");
     }
 
     public void clickable(View v){
@@ -24,29 +26,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
+        Log.d("LifeCycle", "onStart ");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
+        Log.d("LifeCycle", "onResume ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
+        Log.d("LifeCycle", "onPause ");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
+        Log.d("LifeCycle", "onStop ");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.d("LifeCycle", "onDestroy ");
     }
 }
